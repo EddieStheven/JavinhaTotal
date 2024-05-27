9da066089759665486f4b0eee5203a6c3856b7b5
EddieStheven <eddiesthevengith@gmail.com>
2024-05-27 10:09:22 -0300

    At.1

--- src/Treinamento/Java/Insano/javacore/ONE/Desafio1/Back/Áudio.java
+++ src/Treinamento/Java/Insano/javacore/ONE/Desafio1/Back/Áudio.java
@@ -9,6 +9,9 @@ public abstract class Áudio implements Comandos {
     private int totalDeReproducao;
     private int curtidas;
     private double classificacao;
+    private Service service;
+
+
 
     public void imprime(){
         System.out.println("===========================================");
@@ -16,38 +19,43 @@ public abstract class Áudio implements Comandos {
         System.out.println("Suas informações atual: ");
         System.out.println("Você curtiu essa música: " + this.getCurtidas());
         System.out.println("Você reproduziu essa música no total: " + this.getTotalDeReproducao());
+        service();
     }
 
-
-
+    public void service(){
+        if (this.service != null ){
+            System.out.println("Você está escutando no: " + service.getNome());
+        }
+    }
 
     public String getTitulo() {
         return titulo;
     }
-
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
     public int getDuracao() {
         return duracao;
     }
-
     public void setDuracao(int duracao) {
         this.duracao = duracao;
     }
-
     public int getTotalDeReproducao() {
         return totalDeReproducao;
     }
-
     public int getCurtidas() {
         return curtidas;
     }
-
     public double getClassificacao() {
         return classificacao;
     }
+    public Service getService() {
+        return service;
+    }
+    public void setService(Service service) {
+        this.service = service;
+    }
+
 
     @Override
     public void curte() {
